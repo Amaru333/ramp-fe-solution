60b754779198e32ad287951cbb1dd2412ca70e7d
Amaru333 <amrutesharun0599@gmail.com>
2025-02-15 23:03:07 -0500

    Finished

---
+++ src/components/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useState } from "react"
+import { useCustomFetch } from "src/hooks/useCustomFetch"
+import { SetTransactionApprovalParams } from "src/utils/types"
+import { TransactionPane } from "./TransactionPane"
+import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
+
+export const Transactions: TransactionsComponent = ({ transactions }) => {
+  const { fetchWithoutCache, loading } = useCustomFetch()
+
+  const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
+    async ({ transactionId, newValue }) => {
+      await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
+        transactionId,
+        value: newValue,
+      })
+    },
+    [fetchWithoutCache]
+  )
+
+  const [checkedTransactions, setCheckedTransactions] = useState<{ [key: string]: boolean }>({})
+
+  if (transactions === null) {
+    return <div className="RampLoading--container">Loading...</div>
+  }
+
+  return (
+    <div data-testid="transaction-container">
+      {transactions.map((transaction) => (
+        <TransactionPane
+          key={transaction.id}
+          transaction={transaction}
+          loading={loading}
+          setTransactionApproval={setTransactionApproval}
+          checkedTransactions={checkedTransactions}
+          setCheckedTransactions={setCheckedTransactions}
+        />
+      ))}
+    </div>
+  )
+}
